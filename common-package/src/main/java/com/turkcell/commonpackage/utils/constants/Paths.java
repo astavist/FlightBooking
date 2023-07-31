f2762230a9c325a5e0c0468008c15c092e0756d8
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-07-31 21:15:48 +0300

    Added ModelMapper

---
+++ common-package/src/main/java/com/turkcell/commonpackage/utils/constants/Paths.java
@@ -0,0 +1,8 @@
+package com.turkcell.commonpackage.utils.constants;
+
+public class Paths {
+    public static final String ConfigurationBasePackage = "com.turkcell.commonpackage.configuration";
+    public static class Flights {
+        public static final String ServiceBasePackage = "com.turkcell.flightsservice";
+    }
+}
