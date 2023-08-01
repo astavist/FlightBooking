3bc3f167b9860118fd78b962be79be00bd628f0a
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-08-01 23:48:55 +0300

    Added Regex

---
+++ common-package/src/main/java/com/turkcell/commonpackage/utils/constants/DateRegex.java
@@ -0,0 +1,5 @@
+package com.turkcell.commonpackage.utils.constants;
+
+public class DateRegex {
+    public static final String dateRegex = "\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}";
+}
---
+++ common-package/src/main/java/com/turkcell/commonpackage/utils/constants/TimeRegex.java
@@ -0,0 +1,5 @@
+package com.turkcell.commonpackage.utils.constants;
+
+public class TimeRegex {
+    public static final String timeRegex = "^[0-2][0-3]:[0-5][0-9]$";
+}
